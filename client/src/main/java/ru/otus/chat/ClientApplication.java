f721ab4816279a78da218ba8dcc68d4eb129b7c8
Arsakhanov Akhmedkhan <amahan.ru@gmail.com>
2025-04-21 12:14:52 +0300

    chat2025 hw (init)

--- client/src/main/java/ru/otus/chat/ClientApplication.java
+++ client/src/main/java/ru/otus/chat/ClientApplication.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public class ClientApplication {
     public static void main(String[] args) {
+        //доработка
         try {
             new Client();
         } catch (IOException e) {
